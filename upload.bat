ba48015d79c573eebd77ffb3cdc5024418e9633b
Evgeniy <e.pogrebnyak@gmail.com>
2019-12-12 14:30:20 +0300

    windows script for pypi upload

---
+++ upload.bat
@@ -0,0 +1,10 @@
+python setup.py sdist bdist_wheel
+
+REM will prompt for password unless stored locally in a config file
+REM config file location: notepad %userprofile%/.pypirc
+twine upload dist/*
+
+REM Cleaning up...
+rd build /Q/S
+rd dist /Q/S
+rd boo.egg-info /Q/S
