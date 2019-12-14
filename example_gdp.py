720859c361b9d68f6b9a65f8e1ba5e6adbf9119f
Evgeniy <e.pogrebnyak@gmail.com>
2019-12-14 21:28:52 +0300

    Update example_gdp.py

--- example_gdp.py
+++ example_gdp.py
@@ -22,37 +22,42 @@ def plot_mekko(widths,
                labels = ['USA', 'CHN', 'IND', 'JPN', 'DEU'],
                color='dodgerblue',
                edgecolor = 'lightblue'):
-    df = pd.DataFrame(dict(w=widths, h=heights)) \
-            .dropna() \
-            .sort_values('w') \
-            .tail(nlargest) \
-            .sort_values('h')
-    plt.bar(x=[0]+df.w.cumsum().tolist()[:-1],
-            height=df.h,
-            width=df.w,
+    plt.bar(x=[0]+widths.cumsum().tolist()[:-1],
+            height=heights,
+            width=widths,
             align='edge',
             color=color,
             edgecolor=edgecolor)
     plt.title(title)
-    y_shift = df.h.max() * .025
+    y_shift = heights.max() * .025
     for lab in labels:
-        z = df.w.cumsum()
+        z = widths.cumsum()
         bx = z.index.get_loc(lab)
         b = z.iloc[bx,] 
         ax = bx-1       
         a = 0 if bx == 0 else z.iloc[ax,]        
         plt.text(x=(a+b)/2,
-                 y=df.h.loc[lab,] + y_shift,
+                 y=heights.loc[lab,] + y_shift,
                  s=lab,
                  horizontalalignment='center')
     return plt.Rectangle((0, 0), 1, 1, fc=color)    
     
 
-def plot_growth_mekko(source, year, **kwargs):
-    return plot_mekko(w.gdp_usd(year),
-               w.gdp_growth(year),
-               f'GDP growth rates vs base economy size in 2024 and 2018',
-               **kwargs)
+def large_economies(w, year, nlargest):
+    d_ = dict(w=w.gdp_usd(year), h=w.gdp_growth(year))
+    return pd.DataFrame(d_) \
+            .dropna() \
+            .sort_values('w') \
+            .tail(nlargest) \
+            .sort_values('h')
+
+def plot_growth_mekko(source, year, nlargest=25, **kwargs):
+    df = large_economies(w, year, nlargest)
+    return plot_mekko(widths=df.w, 
+                      heights=df.h,
+                      title=f'Largest economies GDP growth rates in 2024 and 2018',
+                      **kwargs)
+
 
 ax = plt.figure()
 ar = plot_growth_mekko(w, 2018, color='lavender',
@@ -64,10 +69,3 @@ plt.ylim(-3,9)
 plt.xlabel("GDP, USD bln")
 plt.ylabel("GDP real growth rate, %")
 plt.legend([ar, br], ["2018", "2024"])
-
-
-#legend((line1, line2, line3), ('label1', 'label2', 'label3'))    
-
-#df = pd.DataFrame(dict(g24=w.gdp_growth(2024), 
-#                  g18=w.gdp_growth(2014), 
-#                 s=w.gdp_usd(2018)))
