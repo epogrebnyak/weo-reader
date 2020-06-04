feb3370d04e4582b90159b8705a00477cf620c48
Evgeniy <e.pogrebnyak@gmail.com>
2020-06-04 18:23:38 +0300

    Create aviz.py

---
+++ example/aviz.py
@@ -0,0 +1,24 @@
+"""
+AutoViz example, see https://github.com/AutoViML
+"""
+import os
+from weo import download, WEO
+
+from autoviz.AutoViz_Class import AutoViz_Class
+
+AV = AutoViz_Class()
+
+if not os.path.exists("weo.csv"):
+   download(2019, 2, "weo.csv")
+w = WEO("weo.csv")
+dft = AV.AutoViz(
+    "",
+    depVar="NGDP_RPCH",
+    dfte=w.fix_year(2024).T,
+    header=0,
+    verbose=2,
+    lowess=False,
+    chart_format="svg",
+    max_rows_analyzed=1000,
+    max_cols_analyzed=30,
+)
