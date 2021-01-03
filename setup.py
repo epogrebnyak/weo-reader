bdd1151e68f1e2f430a6924ef553dbdc7a63fec0
Evgeniy <e.pogrebnyak@gmail.com>
2021-01-03 10:21:29 +0300

    0.0.5 version by @aneziac

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ with open("README.md", encoding="utf-8") as file:
 # 0.1.2020 - change of download args and fixes for 2020 April data
 # 0.3.0 - accept date as YYYY-MM
 # 0.4.0 - dates() and all_dates() functions
-
+# 0.5.0 - new IMF website structure from Oct 2020 on by @aneziac
 setup(
     name="weo",
-    version="0.4.0",
+    version="0.5.0",
     description="Python client to read IMF WEO dataset as pandas dataframe",
     url="http://github.com/epogrebnyak/weo-reader",
     author="Evgeniy Pogrebnyak",
